863cea16855c1f3e78e2f5408706013179dae8ce
Romit2004 <romitguha7@gmail.com>
2024-10-20 13:25:30 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/messages/Message.jsx 	new file:   frontend/src/components/messages/MessageContainer.jsx 	new file:   frontend/src/components/messages/MessageInput.jsx 	new file:   frontend/src/components/messages/Messages.jsx 	new file:   frontend/src/components/sidebar/Conversation.jsx 	new file:   frontend/src/components/sidebar/Conversations.jsx 	new file:   frontend/src/components/sidebar/LogoutButton.jsx 	new file:   frontend/src/components/sidebar/SearchInput.jsx 	new file:   frontend/src/components/sidebar/Sidebar.jsx 	modified:   frontend/src/index.css 	new file:   frontend/src/pages/home/Home.jsx 	renamed:    frontend/src/components/login/Login.jsx -> frontend/src/pages/login/Login.jsx 	renamed:    frontend/src/components/signup/GenderCheckbox.jsx -> frontend/src/pages/signup/GenderCheckbox.jsx 	renamed:    frontend/src/components/signup/Signup.jsx -> frontend/src/pages/signup/Signup.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
-import Login from './components/login/Login'
-import Signup from './components/signup/Signup'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-<Signup />
+      <Home />
     </div>
   )
 }
