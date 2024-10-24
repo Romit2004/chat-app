0ab6f77c2cf48873f3a348a56ea16d08b572e336
Romit2004 <romitguha7@gmail.com>
2024-10-24 20:33:58 +0530

    signup added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,19 @@ import './App.css'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Home from './pages/home/Home'
+import { Routes, Route } from 'react-router-dom'
+import {Toaster} from "react-hot-toast"
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+      <Toaster />
     </div>
   )
 }
